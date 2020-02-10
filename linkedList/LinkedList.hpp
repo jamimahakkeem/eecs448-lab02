bbc4db3a2e59b8927dec94cfe7e6ceb1f280b53c
Jamima Abdul Hakkeem <19jamima97@ku.edu>
2020-02-10 09:02:43 -0600

    Implement search function

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -38,9 +38,10 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO
-		Fix this method
-	*/
+if (value==temp->getValue())
+{
+	isFound=true;
+}
 
 	return(isFound);
 }
